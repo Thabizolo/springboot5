e7a59dab8681f468c304cfae97d08dc082254495
Phumlele Mthembu <phumlelemthembu@Phumleles-MacBook-Pro.local>
2017-08-08 08:29:09 +0200

    adding JPA example

---
+++ src/main/java/guru/springframework/spring5webapp/model/Author.java
@@ -0,0 +1,71 @@
+package guru.springframework.spring5webapp.model;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    @ManyToMany(mappedBy = "authors")
+    private Set<Book> books = new HashSet<>();
+
+    public Author(){
+
+    }
+
+
+    public Author(String firstName, String lastName){
+        this.setFirstName(firstName);
+        this.setLastName(lastName);
+    }
+
+    public Author(String firstName, String lastName, Set<Book> books){
+        this.setFirstName(firstName);
+        this.setLastName(lastName);
+        this.setBooks(books);
+
+    }
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+
+
+}
